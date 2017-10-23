07a22447f5a75f22e2dbacbe05703cf5654ce3f3
shirry0129 <shimizy35@gmail.com>
2017-10-23 09:22:07 +0900

    fix Stack_class(add method)

--- Stack_class/src/Stack.java
+++ Stack_class/src/Stack.java
@@ -2,4 +2,49 @@
 public class Stack {
 	int volume;
 	int data[]=new int[5];
+
+	//データ追加関数
+	boolean push(int number)
+	{
+		if(volume < data.length)
+		{
+			data[volume] = number;
+			volume++;
+			return true;
+		}
+		else
+		{
+			System.out.println("stack overflow");
+			return false;
+		}
+	}
+
+	//データ取得関数
+	int pop()
+	{
+		int value;
+		if(volume > 0)
+		{
+			value = data[volume -1];
+			data[volume -1] = 0;
+			volume--;
+		}
+		else
+		{
+			value = -1;
+		}
+		return value;
+	}
+
+	//状態表示関数
+	void printStack()
+	{
+		System.out.print("|");
+		for(int i=0; i < data.length; i++)
+		{
+			System.out.print(data[i]);
+			System.out.print("|");
+		}
+		System.out.println("");
+	}
 }
