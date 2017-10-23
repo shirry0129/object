cd17b6f73d2c41e119ef0378ded73ddbc2f6a09f
shirry0129 <shimizy35@gmail.com>
2017-10-23 09:32:22 +0900

    fix Queue_class(add method)

--- Queue_class/src/Queue.java
+++ Queue_class/src/Queue.java
@@ -2,4 +2,52 @@
 public class Queue {
 	int volume;
 	int data[]=new int[5];
+
+	//データ追加関数
+	void enqueue(int number)
+	{
+		if(data.length > volume)
+		{
+			data[volume] = number;
+			volume++;
+			System.out.println(number + " inserted");
+		}
+		else
+		{
+			System.out.println("over flow!");
+		}
+	}
+
+	//データ取得関数
+	int dequeue()
+	{
+		int returnValue = 0;
+		if(volume > 0)
+		{
+			returnValue = data[0];
+			for(int i=0;i<volume-1;i++) {
+				data[i]=data[i+1];
+			}
+			data[volume-1]=0;
+			volume--;
+		}
+		else
+		{
+			System.out.println("under flow!");
+			returnValue = -1;
+		}
+		return returnValue;
+	}
+
+	//状態表示関数
+	void printStack()
+	{
+		System.out.print("|");
+		for(int i=0; i < data.length; i++)
+		{
+			System.out.print(data[i]);
+			System.out.print("|");
+		}
+		System.out.println("");
+	}
 }
