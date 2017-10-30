666291e776090902e9d3293bcbe1047ac69a8dcd
shirry0129 <shimizy35@gmail.com>
2017-10-30 09:34:15 +0900

    fix Stack_class(add staticValue)

--- Stack_class/src/Main.java
+++ Stack_class/src/Main.java
@@ -1,24 +1,27 @@
 
 public class Main {
-//	public static void main(String args[]) {
-//		Stack tower = new Stack();
-//		tower.push(10);tower.printStack();
-//		tower.push(20);tower.printStack();
-//		tower.push(30);tower.printStack();
-//		tower.push(40);tower.printStack();
-//		tower.push(50);tower.printStack();
-//		tower.push(60);tower.printStack();
-//		System.out.println(tower.pop());tower.printStack();
-//		System.out.println(tower.pop());tower.printStack();
-//		System.out.println(tower.pop());tower.printStack();
-//		System.out.println(tower.pop());tower.printStack();
-//		System.out.println(tower.pop());tower.printStack();
-//		System.out.println(tower.pop());tower.printStack();
-//	}
+	/*public static void main(String args[]) {
+		Stack tower = new Stack();
+		tower.push(10);tower.printStack();
+		tower.push(20);tower.printStack();
+		tower.push(30);tower.printStack();
+		tower.push(40);tower.printStack();
+		tower.push(50);tower.printStack();
+		tower.push(60);tower.printStack();
+		//tower.data[3]=100;
+		//tower.printStack(3);
+		System.out.println(tower.pop());tower.printStack();
+		System.out.println(tower.pop());tower.printStack();
+		System.out.println(tower.pop());tower.printStack();
+		System.out.println(tower.pop());tower.printStack();
+		System.out.println(tower.pop());tower.printStack();
+		System.out.println(tower.pop());tower.printStack();
+		tower.push(-1);
+	}*/
 	public static void main(String args[]) {
 		Stack tower = new Stack();
-		System.out.println("スタックサイズは"+tower.data.length);
+		System.out.println(Stack.stackVolume()+"個目のスタックです");
 		Stack tower10 = new Stack(10);
-		System.out.println("スタックサイズは"+tower10.data.length);
+		System.out.println(Stack.stackVolume()+"個目のスタックです");
 	}
 }
--- Stack_class/src/Stack.java
+++ Stack_class/src/Stack.java
@@ -1,38 +1,44 @@
 
 public class Stack {
-	int volume;
-	int data[];
+	private int volume;
+	private int data[];
 	static int defaultSize=5;
+	static int stackCount=0;
 
 	//デフォルトコンストラクタ
-	Stack(){
-		data=new int[defaultSize];
-		System.out.println(data.length+"個分のスタック生成");
+	public Stack(){
+		this(defaultSize);
 	}
 
-	Stack(int stackSize){
+	public Stack(int stackSize){
 		data=new int[stackSize];
 		System.out.println(data.length+"個分のスタック生成");
+		stackCount++;
 	}
 
 	//データ追加関数
-	boolean push(int number)
+	public boolean push(int number)
 	{
-		if(volume < data.length)
-		{
-			data[volume] = number;
-			volume++;
-			return true;
-		}
-		else
-		{
-			System.out.println("stack overflow");
+		if(number>0) {
+			if(volume < data.length)
+			{
+				data[volume] = number;
+				volume++;
+				return true;
+			}
+			else
+			{
+				System.out.println("stack overflow");
+				return false;
+			}
+		}else {
+			System.out.println("input value invalid");
 			return false;
 		}
 	}
 
 	//データ取得関数
-	int pop()
+	public int pop()
 	{
 		int value;
 		if(volume > 0)
@@ -49,20 +55,23 @@ public class Stack {
 	}
 
 	//状態表示関数(全体)
-	void printStack()
+	public void printStack()
 	{
 		System.out.print("|");
 		for(int i=0; i < data.length; i++)
 		{
-			System.out.print(data[i]);
+			printStack(i);
 			System.out.print("|");
 		}
 		System.out.println("");
 	}
 
 	//状態表示関数(単体)
-	void printStack(int i) {
-		System.out.print('|');
-		System.out.println(data[i]+"|");
+	private void printStack(int i) {
+		System.out.print(data[i]);
+	}
+
+	public static int stackVolume() {
+		return stackCount;
 	}
 }
