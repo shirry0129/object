780a822150253d8ad438464406daabe04e289ac9
shirry0129 <shimizy35@gmail.com>
2017-11-29 18:10:18 +0900

    add Truck
    Truckクラスの追加

---
+++ Car/src/Truck.java
@@ -0,0 +1,45 @@
+
+public class Truck extends Car{
+	private int nimotsu;
+
+	public int getNimotsu() {
+		return nimotsu;
+	}
+
+	public void setNimotsu(int nimotsu) {
+		this.nimotsu = nimotsu;
+		if(nimotsu>20) {
+			System.out.println("最大積載量が多すぎます.1tに設定します.");
+			this.nimotsu=10;
+		}
+		System.out.println("最大積載量:"+this.nimotsu+"[t]");
+	}
+
+	public Truck() {
+		System.out.println("トラック作成");
+	}
+
+	@Override
+	public void setGas() {
+		gas = 70;
+	}
+
+	@Override
+	public void setNenpi() {
+		nenpi = 13;
+	}
+
+	@Override
+	public void setSpeed(int speed) {
+		this.speed = speed;
+		if(speed>150) {
+			System.out.println("最高速度が速すぎます.120km/hに設定します.");
+			this.speed=120;
+		}
+		if(speed<80) {
+			System.out.println("最高速度が遅すぎます.120km/hに設定します.");
+			this.speed=120;
+		}
+		System.out.println("最高速度:"+this.speed+"[km/h]");
+	}
+}
