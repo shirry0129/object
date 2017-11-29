b7808173c8f33713a1c3ecf9af971c68dd85556a
shirry0129 <shimizy35@gmail.com>
2017-11-29 17:41:52 +0900

    add Car/Car_Main
    Carクラスの追加

---
+++ Car/src/Car.java
@@ -0,0 +1,111 @@
+
+public abstract class Car {
+	private int speed;
+	private int tire;
+	private int door;
+	private int gas;
+	private int mileage;
+	private static int base_speed=100;
+	private static int base_door=5;
+	private static int base_tire=4;
+
+	//デフォルトコンストラクタ
+	Car(){
+		this(base_speed,base_door,base_tire);
+	}
+
+	//コンストラクタ
+	Car(int speed,int tire,int door){
+		System.out.println("車作成");
+		setSpeed(speed);
+		setTire(tire);
+		setDoor(door);
+	}
+
+	//最高速度の取得
+	public int getSpeed() {
+		return speed;
+	}
+
+	//タイヤの数の取得
+	public int getTire() {
+		return tire;
+	}
+
+	//ドア数の取得
+	public int getDoor() {
+		return door;
+	}
+
+	//燃料数の取得
+	public int getGas() {
+		return gas;
+	}
+
+	//燃費の取得
+	public int getMileage() {
+		return mileage;
+	}
+
+	//最高速度の設定
+	public void setSpeed(int speed) {
+		this.speed = speed;
+		if(speed>200) {
+			System.out.println("最高速度が速すぎます.150km/hに設定します.");
+			this.speed=150;
+		}
+		if(speed<100) {
+			System.out.println("最高速度が遅すぎます.150km/hに設定します.");
+			this.speed=150;
+		}
+		System.out.println("最高速度:"+this.speed+"[km/h]");
+	}
+
+	//タイヤの数の設定
+	public void setTire(int tire) {
+		this.tire = tire;
+		if(tire<4) {
+			System.out.println("タイヤの数が少なすぎます.4輪に設定します.");
+			this.tire=4;
+		}
+		if(tire>6) {
+			System.out.println("タイヤの数が多すぎます.4輪に設定します.");
+			this.tire=4;
+		}
+		System.out.println("タイヤ:"+this.tire+"輪");
+	}
+
+	//ドア数の設定
+	public void setDoor(int door) {
+		this.door = door;
+		if(door<2) {
+			System.out.println("ドアの数が少なすぎます.5ドアに設定します.");
+			this.door=5;
+		}
+		if(door>5) {
+			System.out.println("ドアの数が多すぎます.5ドアに設定します.");
+			this.door=4;
+		}
+		System.out.println("ドア数:"+this.door+"ドア");
+	}
+
+	//燃料数の設定(abstract)
+	public abstract void setGas();
+
+	//燃費の設定(abstract)
+	public abstract void setMileage();
+
+	public void drive() {
+		if(getGas()!=0) {
+			System.out.print("ドドド");
+			if(gas > mileage) {
+				gas -= mileage;
+			}else {
+				gas = 0;
+			}
+			System.out.println("残燃料:"+getGas());
+		}else {
+			System.out.println("ガス欠です");
+		}
+	}
+}
---
+++ Car/src/Car_Main.java
@@ -0,0 +1,7 @@
+
+public class Car_Main {
+	public static void main(String[] args) {
+
+
+	}
+}
